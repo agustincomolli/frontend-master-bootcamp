c4497e1b18e2f97c07ed294940705c75cbd207b9
Agustín Comolli <agustincomolli@gmail.com>
2025-09-18 13:36:10 -0300

    add primitives datatypes

--- dia-06/script.js
+++ dia-06/script.js
@@ -1,13 +1,21 @@
+console.log("DATOS PRIMITVOS\n" + "*".repeat(15));
+
+console.log("\nNúmeros: ");
 const monthlyRent = 500;
 let yearlyRent = monthlyRent * 12;
 
-console.log(monthlyRent, yearlyRent);
+console.log("- ", monthlyRent, yearlyRent);
 
 yearlyRent = 1;
-console.log(yearlyRent);
+console.log("- ", yearlyRent);
 
 
+console.log("\nCadenas de texto:");
 const firstName = "Agustín";
 const lastName = "Comolli";
 const greeting = `¡Hola, ${firstName} ${lastName}! ¿Cómo estás?`;
-console.log(greeting);
+console.log("- ", greeting);
+
+console.log("\nBooleanos");
+const isTheSkyBlue = true;
+console.log("- ", isTheSkyBlue);
